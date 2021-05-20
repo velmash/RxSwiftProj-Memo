717fe3774ae5535ef383d2282448425e18e56c1a
hyengchan <hyengchan@39degreesc.com>
2021-05-20 10:00:59 +0900

    implement MemoDetailViewModel

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -21,6 +21,24 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
           
      }
      func bindViewModel() {
+          viewModel.title
+               .drive(navigationItem.rx.title)
+               .disposed(by: rx.disposeBag)
           
+          viewModel.content
+               .bind(to: listTableView.rx.items) { tableview, row, value in
+                    switch row {
+                    case 0:
+                         let cell = tableview.dequeueReusableCell(withIdentifier: "contentCell")!
+                         cell.textLabel?.text = value
+                         return cell
+                    case 1:
+                         let cell = tableview.dequeueReusableCell(withIdentifier: "dateCell")!
+                         cell.textLabel?.text = value
+                    default:
+                         fatalError()
+                    }
+               }
+               .disposed(by: rx.disposeBag)
      }
 }
--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -6,7 +6,31 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Action
 
-class MemoDetailViewModel {
+class MemoDetailViewModel: CommonViewModel {
+     let memo: Memo
      
+     private var formatter: DateFormatter = {
+          let f = DateFormatter()
+          f.locale = Locale(identifier: "Ko_kr")
+          f.dateStyle = .medium
+          f.timeStyle = .medium
+          return f
+     } ()
+     
+     var content: BehaviorSubject<[String]>
+     
+     init(memo: Memo, title: String, sceneCoordinator: SceneCoordinatorType, storage: MemoStorageType) {
+          self.memo = memo
+          
+          content = BehaviorSubject<[String]>(value: [
+               memo.content,
+               formatter.string(from: memo.insertDate)
+          ])
+          
+          super.init(title: title, sceneCoordinator: sceneCoordinator, storage: storage)
+     }
 }
