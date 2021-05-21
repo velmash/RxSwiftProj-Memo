32cd4acc3c041c72e5dbafdd2419d92c75500db2
hyengchan <hyengchan@39degreesc.com>
2021-05-21 10:23:38 +0900

    fix editing issue

--- RxMemo/ViewModel/MemoDetailViewModel.swift
+++ RxMemo/ViewModel/MemoDetailViewModel.swift
@@ -11,7 +11,7 @@ import RxCocoa
 import Action
 
 class MemoDetailViewModel: CommonViewModel {
-     let memo: Memo
+     var memo: Memo
      
      private var formatter: DateFormatter = {
           let f = DateFormatter()
@@ -42,6 +42,9 @@ class MemoDetailViewModel: CommonViewModel {
           return Action { input in
                self.storage.update(memo: memo, content: input)
                     .subscribe(onNext: { updated in
+                         
+                         self.memo = updated
+                         
                          self.content.onNext([
                               updated.content,
                               self.formatter.string(from: updated.insertDate)
